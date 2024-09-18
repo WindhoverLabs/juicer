3f15cc090f12693c087073328dedbbcae2c84b66
lgomez <lgomez@windhoverlabs.com>
2024-09-18 10:16:16 -0500

    -Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -316,9 +316,6 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
     return 0;
 }
 
-TestStructA_t      testStructA2 = {};
-TestStructB_t      testStructB2 = {};
-
 /* Our argp parser. */
 static struct argp argp         = {options, parse_opt, args_doc, doc};
 
